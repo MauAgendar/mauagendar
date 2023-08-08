5d51d93a0c6955b67c48a2433f2f309e1a2a6ced
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2023-08-08 17:54:18 -0300

    Atualizando publishAuthEvent.ts e removendo arquivo de stack nao utilizado

--- auth/src/controller/publishAuthEvent.ts
+++ auth/src/controller/publishAuthEvent.ts
@@ -1,21 +1,46 @@
 import amqp from "amqplib";
+
 const amqpUrl = process.env.AMQP_URL || "amqp://guest:guest@rabbitmq:5672/";
+
+const retryOptions = {
+    retries: 5, // Number of retries
+    delay: 1000, // Initial delay in milliseconds
+    factor: 2, // Delay factor for exponential backoff
+};
+
+const retry: any = async (fn: () => Promise<any>, options: any) => {
+    try {
+        return await fn();
+    } catch (error) {
+        if (options.retries <= 0) {
+            throw error;
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, options.delay));
+        options.retries--;
+        options.delay *= options.factor;
+
+        return retry(fn, options);
+    }
+};
 
 export const publishAuthenticationEvent = async (token: string) => {
     try {
-        const connection = await amqp.connect(amqpUrl);
-        const channel = await connection.createChannel();
-        const queue = "auth_queue";
+        await retry(async () => {
+            const connection = await amqp.connect(amqpUrl);
+            const channel = await connection.createChannel();
+            const queue = "auth_queue";
 
-        await channel.assertQueue(queue);
-        const message = JSON.stringify({ token }); // Updated payload to include the token
-        console.log(message);
+            await channel.assertQueue(queue);
+            const message = JSON.stringify({ token });
+            console.log(message);
 
-        channel.sendToQueue(queue, Buffer.from(message));
-        console.log("Authentication event published");
+            channel.sendToQueue(queue, Buffer.from(message));
+            console.log("Authentication event published");
 
-        await channel.close();
-        await connection.close();
+            await channel.close();
+            await connection.close();
+        }, retryOptions);
     } catch (err) {
         console.error("Error publishing authentication event", err);
     }
