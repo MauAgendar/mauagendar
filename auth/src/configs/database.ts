97a38addb826bfb9a214a9a5cd3769c6a79fb753
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2023-08-26 11:37:56 -0300

    URLs render

--- auth/src/configs/database.ts
+++ auth/src/configs/database.ts
@@ -3,11 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const client = new Client({
-    host: process.env.PG_HOST,
-    port: Number(process.env.PG_PORT),
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
+    url: process.env.DATABASE_URL,
 });
 
 export default client;
