32150863897e48a4a5487bb77deea86a781f7e94
EnricoGia <rico500000@gmail.com>
2023-10-23 19:19:11 -0300

    Adicionando cores globais

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -6,6 +6,12 @@ void main() {
   runApp(const LoginApp());
 }
 
+Color primaryColor = const Color.fromARGB(255, 66, 37, 184);
+Color secondaryColor = const Color.fromARGB(255, 19, 6, 25);
+Color accentColor = const Color.fromARGB(255, 146, 46, 189);
+Color textColor = const Color.fromARGB(255, 251, 252, 254);
+Color backgroundColor = const Color.fromARGB(255, 9, 17, 37);  
+
 class LoginApp extends StatelessWidget {
   const LoginApp({super.key});
 
@@ -20,7 +26,7 @@ class LoginApp extends StatelessWidget {
         theme: ThemeData(
             useMaterial3: true,
             colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.deepPurple.shade900,
+              seedColor: Colors.white,
               // ···
               brightness: Brightness.dark,
             ),
